9e0011ecefab67f5596966b005055a1604bc4a57
christ <christayena@gmail.com>
2026-07-25 22:25:40 +0100

    Corriger le rechargement forcé sur la page de connexion

--- src/services/api.js
+++ src/services/api.js
@@ -31,7 +31,9 @@ api.interceptors.response.use(
     
     if (response?.status === 401) {
       localStorage.removeItem('token')
-      window.location.href = '/login'
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login'
+      }
       return Promise.reject(error)
     }
     
